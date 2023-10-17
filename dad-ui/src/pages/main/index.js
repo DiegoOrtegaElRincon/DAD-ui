db2b351af612a3fcc4d7b4dc8b32680f0063c261
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-10-17 21:41:22 +0200

    1st Commit

---
+++ dad-ui/src/pages/main/index.js
@@ -0,0 +1,6 @@
+import NavBar from '../../components/topNavBar/navbar'
+export default function MainPage() {
+    return (
+        <NavBar props='Competitive Matches'/>
+    )
+}
---
+++ dad-ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/main/index.js',
+    './components/topNavBar/navbar.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
